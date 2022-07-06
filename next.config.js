fb624bf97254206ca82a49adbe8c3d1db2564e8f
Arpit Singh <f20190956@pilani.bits-pilani.ac.in>
2022-07-07 00:38:08 +0530

    changed config for upload to IPFS

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    images: {
+        loader: "akamai",
+        path: "",
+    },
 }
 
 module.exports = nextConfig
