347d0c15050bce72db73a4ce30280718b23d46c6
Shiv Bhonde <shivbhonde04@gmail.com>
2022-07-21 02:40:44 +0530

    update domains for images in config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
     images: {
         loader: "akamai",
         path: "",
+        domains: [
+            "ipfs.io",
+            "ipfs.fleek.co",
+            "res.cloudinary.com",
+            "ipfs.fleek.io",
+            "cloudflare-ipfs.com",
+            "arweave.net",
+            "ipfs.infura.io",
+        ],
     },
 }
 
