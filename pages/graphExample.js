61391f07e862296250f684cb722831d7f8ca2d31
nagrarohit <rohit.nagra7861@gmail.com>
2023-05-13 16:38:54 +0530

    Update graphExample.js

--- pages/graphExample.js
+++ pages/graphExample.js
@@ -2,7 +2,7 @@ import { useQuery, gql } from "@apollo/client"
 
 const GET_ACTIVE_ITEMS = gql`
     {
-        activeItems(first: 5, where: { buyer: "0x00000000" }) {
+        activeItems(first: 5, where: { buyer: "0x0000000000000000000000000000000000000000" }) {
             id
             buyer
             seller
